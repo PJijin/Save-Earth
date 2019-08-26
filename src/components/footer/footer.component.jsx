6928217d5df3a55b2342d76a45bb0ef5e8740300
Pjijin <Pjijin1@gmail.com>
2019-08-26 21:28:19 +0530

    Added footer links

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { GitHub } from 'react-feather';
 import { ReactComponent as Heart } from './heart.svg';
-
 export default function Footer() {
 	return (
-		<div className="footer container">
-			Made on Earth by Humans with <Heart width="18px" />
-			<a target="_BLANK" rel="noreferrer noopener" href="https://twitter.com/JP1016v1">
-				@JP1016v1
-			</a>
-			&
-			<a target="_BLANK" rel="noreferrer noopener" href="https://twitter.com/pJijin">
-				@Pjijin
-			</a>
+		<div className="footer flex container">
+			<div className="credits">
+				Made on Earth by Humans with <Heart width="18px" />
+				<a target="_BLANK" rel="noreferrer noopener" href="https://twitter.com/JP1016v1">
+					@JP1016v1
+				</a>
+				&
+				<a target="_BLANK" rel="noreferrer noopener" href="https://twitter.com/pJijin">
+					@Pjijin
+				</a>
+			</div>
+			<div>
+				<a href="https://github.com/PJijin/Save-Earth" target="_BLANK" rel="noreferrer noopener">
+					<GitHub size="12" /> Source
+				</a>
+			</div>
 		</div>
 	);
 }
